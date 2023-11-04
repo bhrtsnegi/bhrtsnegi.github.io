2e6c620d7bb43a1148b5227d69e1b4f173dd1a48
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-04 22:15:00 +0530

    Js updated

--- project/weather/index.js
+++ project/weather/index.js
@@ -1,45 +1,48 @@
+//Personal Api Key generated from weather api site
 const apiKey= "1c4f6e668580d88a40e4ec3fc26e50ef";
-            const apiUrl= "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
-            const searchBox = document.querySelector(".search input");
-            const searchBtn = document.querySelector(".search button");
-            const weatherIcon = document.querySelector(".weather-icon")
-            async function checkWeather(city){
-                const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-                if(response.status == 404)
-                {
-                    document.querySelector(".error").style.display = "block";
-                    document.querySelector(".weather").style.display = "none";
-                }
-                else
-                {
-                    var data = await response.json();
+const apiUrl= "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 
-                    document.querySelector(".city").innerHTML = data.name;
-                    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-                    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-                    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-
-                    if(data.weather[0].main == "Clouds")
-                        weatherIcon.src = "images/clouds.png";
-
-                    else if(data.weather[0].main == "Clear")
-                        weatherIcon.src ="images/clear.png";
-
-                    else if(data.weather[0].main == "Rain")
-                        weatherIcon.src ="images/rain.png";
-
-                    else if(data.weather[0].main == "Drizzle")
-                        weatherIcon.src ="images/drizzle.png";
+async function checkWeather(city){
                 
-                    else if(data.weather[0].main == "Mist")
-                        weatherIcon.src ="images/mist.png";
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if(response.status == 404)
+    {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else
+    {
+        var data = await response.json();
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-                        document.querySelector(".error").style.display = "none";
-                    document.querySelector(".weather").style.display = "block";
-                }
-            }
 
-            searchBtn.addEventListener("click", ()=>{
-                checkWeather(searchBox.value);
-            })
+        if(data.weather[0].main == "Clouds")
+            weatherIcon.src = "images/clouds.png";
+
+        else if(data.weather[0].main == "Clear")
+            weatherIcon.src ="images/clear.png";
+
+        else if(data.weather[0].main == "Rain")
+            weatherIcon.src ="images/rain.png";
+
+        else if(data.weather[0].main == "Drizzle")
+            weatherIcon.src ="images/drizzle.png";
+    
+        else if(data.weather[0].main == "Mist")
+            weatherIcon.src ="images/mist.png";
+
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+    }
+}
+
+//operation while pressing search button
+searchBtn.addEventListener("click", ()=>{
+checkWeather(searchBox.value);
+})
