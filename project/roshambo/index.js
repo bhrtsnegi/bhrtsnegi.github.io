3de63191d01b0fbd8115b2e69972186c40811af7
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-13 10:08:35 +0530

    done function

--- project/roshambo/index.js
+++ project/roshambo/index.js
@@ -1,15 +1,12 @@
-let computerChoice = '';
-
 function generateComputerChoice(){
     let randomNumber = Math.random()*3;
     if(randomNumber  > 0 && randomNumber<=1){
-        computerChoice = 'Stone';
+        return 'Stone';
     }
     else if(randomNumber  > 1 && randomNumber<=2){
-        computerChoice = 'Paper';
+        return 'Paper';
     }
     else{
-        computerChoice = 'Scissors';
+        return 'Scissors';
     }
-
 }
