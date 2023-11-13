3d7b42e8e7d0bbf898876ba682095aef7a9aefdb
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-13 10:31:45 +0530

    Attached All 3 functions

--- project/roshambo/index.js
+++ project/roshambo/index.js
@@ -10,3 +10,33 @@ function generateComputerChoice(){
         return 'Scissors';
     }
 }
+
+function getResult(userMove, computerMove){
+    if(userMove === 'Stone')
+        if(computerMove === 'Paper')    return 'Computer Won.';
+
+        else if(computerMove === 'Scissors')    return 'You Won.';
+
+        else return `It's a tie`;
+
+    if(userMove === 'Paper')
+        if(computerMove === 'Scissors')    return 'Computer Won.';
+
+        else if(computerMove === 'Stone')    return 'You Won.';
+
+        else return `It's a tie`;
+
+    if(userMove === 'Scissors')
+        if(computerMove === 'Stone')    return 'Computer Won.';
+
+        else if(computerMove === 'Paper')    return 'You Won.';
+
+        else return `It's a tie`;
+
+}
+
+
+function showResult(userMove, computerMove, result)
+{
+    alert(`You have chosen ${userMove}, Computer's choice is ${computerMove} and ${result}`);
+}
