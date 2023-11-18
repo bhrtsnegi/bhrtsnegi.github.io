3310a56e32e46b79cdbb448d1be5faba16a0535f
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-18 11:18:15 +0530

    Products added using items.js

--- project/myntra/scripts/index.js
+++ project/myntra/scripts/index.js
@@ -1,21 +1,23 @@
 let itemsContainerElement = document.querySelector('.items-container');
 
-itemsContainerElement.innerHTML =`
+let innerHtml = '';
+items.forEach(item =>{
+    innerHtml += `
     <div class="item-container">
-    <img class="item-image" src="images/1.jpg" alt="item one">
-    <div class="rating">
-        4.5⭐ | 1.4k
+        <img class="item-image" src="${item.image}" alt="item one">
+        <div class="rating">
+            ${item.rating.stars}⭐ | ${item.rating.count}
+        </div>
+        <div class="company-name">${item.company}</div>
+        <div class="item-name">${item.item_name}</div>
+        <div class="price">
+            <span class="current-price">Rs ${item.current_price}</span>
+            <span class="original-price">Rs ${item.original_price}</span>
+            <span class="discount">(${item.discount_percentage}% OFF)</span>
+        </div>
+        <button class="btn-add-bag">Add to Bag</button>
     </div>
-    <div class="company-name">Cartlon London</div>
-    <div class="item-name">Rhodium-Plated Studs</div>
-    <div class="price">
-        <span class="current-price">Rs 606</span>
-        <span class="original-price">Rs 1045</span>
-        <span class="discount">(42% OFF)</span>
-    </div>
-    <button class="btn-add-bag">Add to Bag</button>
-    </div>
-    `;
+`;
+})
 
-    let item = document.querySelector('.item-container');
-    let itemImage;
+itemsContainerElement.innerHTML = innerHtml;
