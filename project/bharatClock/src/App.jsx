7075d3e5367834e3fbd18a1b6850a0077af10c97
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-20 21:23:16 +0530

    updated content

--- project/bharatClock/src/App.jsx
+++ project/bharatClock/src/App.jsx
@@ -1,6 +1,7 @@
 import ClockTitle from "./components/ClockTitle";
 import ClockText from "./components/ClockText";
 import ClockTime from "./components/ClockTime";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App(){
