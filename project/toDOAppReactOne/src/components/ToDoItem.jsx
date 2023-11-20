6ea1f85470fa52db46bfc4b8738a1e7c950d9ff0
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-20 10:04:53 +0530

    variables defined for values

--- project/toDOAppReactOne/src/components/ToDoItem.jsx
+++ project/toDOAppReactOne/src/components/ToDoItem.jsx
@@ -1,12 +1,15 @@
 function ToDoItem(){
+
+  let taskName = 'Go To College';
+  let taskDate = '04/10/2023';
   return(
     <div className="container text-center">
         <div class="row">
           <div class="col-6">
-            Go To College
+            {taskName}
           </div>
           <div class="col-4">
-            04/10/2023
+            {taskDate}
           </div>
           <div class="col-2">
             <button type="button" class="btn btn-danger">Delete</button>
