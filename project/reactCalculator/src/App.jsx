e19bce921aee4b217a556a4f59d9f76963449277
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-24 10:35:29 +0530

    common updates

--- project/reactCalculator/src/App.jsx
+++ project/reactCalculator/src/App.jsx
@@ -1,14 +1,13 @@
-import styles from './App.module.css'
-import Display from './components/Display'
-import ButtonsContainer from './components/ButtonsContainer'
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
 function App() {
-  
   return (
     <div className={styles.calculator}>
       <Display />
       <ButtonsContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- project/reactCalculator/src/components/ButtonsContainer.jsx
+++ project/reactCalculator/src/components/ButtonsContainer.jsx
@@ -1,14 +1,32 @@
 import styles from "./ButtonsContainer.module.css";
 
-const ButtonsContainer = () =>{
+const ButtonsContainer = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <div className={styles.buttonsContainer}>
-        <button className={styles.button}>C</button>
-        <button className={styles.button}>1</button>
-        <button className={styles.button}>2</button>
-        <button className={styles.button}>+</button>
-      </div>
+      {buttonNames.map((item) => (
+        <button className={styles.button}>{item}</button>
+      ))}
+    </div>
   );
-}
+};
 
 export default ButtonsContainer;
