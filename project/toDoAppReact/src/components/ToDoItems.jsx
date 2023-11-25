c151b0e90c15955d60ee550bcf537a80a1d94334
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-25 20:08:27 +0530

    completed: ToDo App Design

--- project/toDoAppReact/src/components/ToDoItems.jsx
+++ project/toDoAppReact/src/components/ToDoItems.jsx
@@ -1,11 +1,16 @@
 import ToDoItem from "./ToDoItem";
 import styles from "./ToDoItems.module.css";
 
-const ToDoItems = ({ todoItems, onDeleteClick }) =>{
+const ToDoItems = ({ todoItems, onDeleteClick }) => {
   return (
     <div className={styles.itemsContainer}>
       {todoItems.map((item) => (
-      <ToDoItem taskName={item.name} taskDate={item.dueDate} onDeleteClick={onDeleteClick}></ToDoItem>
+        <ToDoItem
+          key={item.name}
+          taskName={item.name}
+          taskDate={item.dueDate}
+          onDeleteClick={onDeleteClick}
+        ></ToDoItem>
       ))}
     </div>
   );
