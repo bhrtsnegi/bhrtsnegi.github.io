1475628ff6f66607e459e6f3442e1549b1e78968
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-27 14:52:41 +0530

    update: converted useState to useRef

--- project/toDoAppReact/src/components/AddToDo.jsx
+++ project/toDoAppReact/src/components/AddToDo.jsx
@@ -1,44 +1,33 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { IoAddCircle } from "react-icons/io5";
 import styles from "./AddToDo.module.css";
 
 function AddToDo({ onNewItem }) {
-  const [toDoName, setToDOName] = useState();
-
-  const [dueDate, setDueDate] = useState();
-
-  const handleNameChange = (event) => {
-    setToDOName(event.target.value);
-  };
-
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
+  const toDoNameElement = useRef();
+  const dueDateElement = useRef();
 
   const handleAtButtonClick = (event) => {
     event.preventDefault();
+    const toDoName = toDoNameElement.current.value;
+    toDoNameElement.current.value = "";
+    const dueDate = dueDateElement.current.value;
+    dueDateElement.current.value = "";
     onNewItem(toDoName, dueDate);
-    setDueDate("");
-    setToDOName("");
   };
   return (
-    <div className="container">
+    <div className="container text-center">
       <form className="row item-row" onSubmit={handleAtButtonClick}>
         <div className="col-6">
           <input
-            className={styles.taskInput}
             type="text"
+            ref={toDoNameElement}
             placeholder="Enter Task Here"
-            value={toDoName}
-            onChange={handleNameChange}
           />
         </div>
         <div className="col-4">
           <input
-            className={styles.taskInput}
             type="date"
-            value={dueDate}
-            onChange={handleDateChange}
+            ref={dueDateElement}
           />
         </div>
         <div className="col-2">
