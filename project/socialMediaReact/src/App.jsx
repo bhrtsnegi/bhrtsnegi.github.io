6129d3c9235c44b596ac15cbacf619a4b3a5472b
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-29 12:10:32 +0530

    created : form for create post

--- project/socialMediaReact/src/App.jsx
+++ project/socialMediaReact/src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Sidebar></Sidebar>
       <div className="content">
         <Header></Header>
+        <CreatePost></CreatePost>
         <Footer></Footer>
       </div>
     </div>
