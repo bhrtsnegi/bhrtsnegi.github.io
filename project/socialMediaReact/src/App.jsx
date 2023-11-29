470eb4f32157075a943b2e905195ff85211ea933
bhrtsnegi <1bhrtsnegi@gmail.com>
2023-11-29 12:15:56 +0530

    added : Post in Postlist for multiple post

--- project/socialMediaReact/src/App.jsx
+++ project/socialMediaReact/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <div className="content">
         <Header></Header>
         <CreatePost></CreatePost>
+        <PostList></PostList>
         <Footer></Footer>
       </div>
     </div>
